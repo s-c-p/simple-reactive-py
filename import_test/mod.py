8450633d98191ab393def59e50882f81436ab6d6
s-c-p <ps45@binkmail.com>
2017-08-13 20:33:48 +0530

    Lazy reactors approach failed
    Start reading oush() function in queue.py and see the comments and notes
    made. Gotta make a better architecture where all objects have exactly
    one instance and are alive and watchin (alas...) a (the) STREAM, they
    act if changes in STREAM are recognizable else ignore.
    
    Additionally, read -- http://bit.ly/2uD5WTL
     On branch master
     Changes to be committed:
    	new file:   import_test/__init__.py
    	new file:   import_test/mod.py
    	new file:   queue.py

---
+++ import_test/mod.py
@@ -0,0 +1,9 @@
+class A:
+    """docstring for A"""
+    def __init__(self, x):
+        self.x = x
+        return
+
+    def func(self):
+	"""call this func from .."""
+        return self.x
---
+++ queue.py
@@ -0,0 +1,83 @@
+import os
+import uuid
+import inspect
+
+# from contextlib import contextmanager
+# 
+# job_list = "jobs.list"
+# physics_rules = "physics.json"
+# 
+# @contextmanager
+# def documentDB(file_name):
+# 	if not os.path.isfile(file_name):
+# 		with open(file_name, mode='wt') as _:	pass
+# 	with open(file_name, mode='rt') as fp:
+# 		con = json.load(fp) 
+# 	yield con
+# 	with open(file_name, mode='wt') as fp:
+# 		json.dump(con, fp)
+# 	return
+
+# sample
+job_list = list()
+physics_rules = {
+	"sender_name_1": [
+		("full_func_path_1", "expected value on success", "log OR raise", "async?"),
+		("full_func_path_2", "expected value on success", "log OR raise", "async?"),
+		("full_func_path_3", "expected value on success", "log OR raise", "async?")
+	],
+	"sender_name_2": [
+		("full_func_path_1", "expected value on success", "log OR raise", "async?"),
+		("full_func_path_2", "expected value on success", "log OR raise", "async?")
+	]
+}
+
+def _defer(params, reactorDetails):
+	func_name = reactorDetails["full_func_path"]
+	args = inspect.getargspec(func_name)
+	# firstly I can't really get argspec of things i haven't imported (PRACTICAL PROBLEM)
+	# even if reactorDetails were to contain a field mentioning all args, namedargs, defaultargs, etc. etc.
+	# how would i call functions without importing
+	# can't just do `import func_name.split(".")[0]` it won't work in cases like from urllib.parser import urlencode
+	return
+
+def defer(params, reactors):
+	"""	async await
+	https://hackernoon.com/asynchronous-python-45df84b82434
+	https://gist.github.com/nhumrich/b53cc1e0482411e4b53d74bd12a485c1#file-gevent_example-py
+	https://medium.freecodecamp.org/a-guide-to-asynchronous-programming-in-python-with-asyncio-232e2afa44f6
+	https://github.com/ask/flask-celery/
+	"""
+	for aReactor in reactors:
+		_defer(message, aReactor)
+	return
+
+def push(message, sender):
+	"""
+	x -- implicitly knows about sender (because it violates the defn of pure functional...)
+	assigns a message_id
+	raises some alert, so everyone becomes concious
+	now the functions which care about message m from sender s
+	do their thing and acknowledge queue about success or failure
+	  failure is logged unless explicitly programmed to halt (using know_that)
+	  all successes are checked against know_that's own list of reactors
+	  any unexpected success raises programmingErr
+	# ans.stimulus()
+	"""
+	message_id = str(uuid.uuid4())
+	jobs.append({message_id: message})
+	wake_everyone()	# HAHA
+	reactor_list = physics_rules[sender]
+	defer(message, reactor_list)
+	return
+
+def pop(message_id):
+	""" when all subscribers have reported success """
+	return
+
+def _know_that(sender, reactor_list):
+	"""	has the responsibility of calling pop()
+	ans.subscribe(speaker)  # does this go into setup.py
+	ans.set(reaction, stimulant)    # one stimulant can have many reactions
+	"""
+	return
