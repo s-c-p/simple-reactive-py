3dd585ba5d24af387b5932a14b69c22ae61491d2
s-c-p <ps45@binkmail.com>
2017-08-15 08:32:15 +0530

    all classes (till line #69) tested working

--- queue.py
+++ queue.py
@@ -4,40 +4,80 @@ import json
 import inspect
 import contextlib
 
-?job_list = "jobs.list"
-?physics_rules = "physics.json"
 
-@contextlib.contextmanager
-def documentDB(file_name):
-	if not os.path.isfile(file_name):
-		with open(file_name, mode='wt') as _:	pass
-	with open(file_name, mode='rt') as fp:
-		con = json.load(fp) 
-	yield con
-	with open(file_name, mode='wt') as fp:
-		json.dump(con, fp)
-	return
 
-class ConfigError(Error):
+JOB_LIST = "jobs.list"
+PHYSICS_RULES = "physics.json"
+
+
+
+class ConfigError(Exception):
 	""" shows error in configuration file """
 	def __init__(self, msg):
 		self.msg = msg
 		return
-	def __str__(self):
-		print(self.msg)
 
 class PhysicsRule():
-	"""docstring for PhysicsRule"""
+	""" struct for storing a rule i.e. details of a reactor """
 	def __init__(self, func_path, func_name, expectedAns, onerror):
 		self.func_path = func_path
 		self.func_name = func_name
 		self.expectedAns = expectedAns
 		self.onerror = onerror
 		return
+
 	@staticmethod
 	def _make(argTuple):
 		a, b, c, d = argTuple
 		return PhysicsRule(a, b, c, d)
+
+class RuleEncoder(json.JSONEncoder):
+	""" add-on to json to help properly encode (dump py obj to file) a \
+	physics rule
+	"""
+	def default(self, theRule):
+		if isinstance(theRule, PhysicsRule):
+			return \
+			{ "func_path": theRule.func_path
+			, "func_name": theRule.func_name
+			, "onerror": theRule.onerror
+			, "expectedAns": theRule.expectedAns }
+		# return json.JSONEncoder.default(self, theRule)
+		return super(RuleEncoder, self).default(theRule)
+
+class RuleDecoder(json.JSONDecoder):
+	""" 'Lambda' para on bit.ly/2vCwlSQ """
+	def __init__(self):
+		# json.JSONDecoder.__init__(self, object_hook=self.dict2obj)
+		super(RuleDecoder, self).__init__(object_hook=self.dict2obj)
+	def dict2obj(self, dicn):
+		keys = dicn.keys()
+		if len(keys) == len(self.NECESSARY_TRAITS) and \
+			all(  [trait in keys for trait in self.NECESSARY_TRAITS]  ):
+			# NOTE: the following (seemingly shortcut)
+			# won't work because dicn keys' positions are not fixed
+			# return PhysicsRule._make(tuple(dicn.values())) 
+			w = dicn["func_path"]
+			x = dicn["func_name"]
+			y = dicn["expectedAns"]
+			z = dicn["onerror"]
+			tup = w, x, y, z
+			return PhysicsRule._make(tup)
+		else:
+			raise ConfigError("")
+	NECESSARY_TRAITS = ["func_path", "func_name", "expectedAns", "onerror"]
+
+@contextlib.contextmanager
+def documentDB(file_name):
+	if not os.path.isfile(file_name):
+		# with open(file_name, mode='wt') as _: pass
+		raise ConfigError(f"configuration file '{file_name}' not found")
+	with open(file_name, mode='rt') as fp:
+		con = json.load(fp)
+	yield con
+	with open(file_name, mode='wt') as fp:
+		json.dump(con, fp, cls=RuleEncoder)
+	return
 
 # sample
 job_list = list()
@@ -66,7 +106,7 @@ def _defer(all_params, reactorDetails):
 	# STEP-2: get argument-related details of the function
 	call_specs = inspect.getfullargspec(func)
 	# TRY: >>> def f(x, y=1, z=2, *args, **kwargs): pass
-	# FullArgSpec(args=['x', 'y', 'z'], varargs='args', varkw='kwargs', 
+	# FullArgSpec(args=['x', 'y', 'z'], varargs='args', varkw='kwargs',
 	#   defaults=(1, 2), kwonlyargs=[], kwonlydefaults=None, annotations={})
 	# NOTE: I really don't know how to use kwonlyargs & kwonlydefaults IF i
 	#   can get a good use case I will implement it
@@ -81,7 +121,7 @@ def _defer(all_params, reactorDetails):
 		# it might be the case that the user didn't supply k-v pairs for
 		#   defaults
 		lastN = len(call_specs.defaults)
-		names = call_specs.args[-1*lastN:]	# get last n elements of list
+		names = call_specs.args[-1*lastN:]  # get last n elements of list
 		temp = dict(zip(names, call_specs.defaults))
 		# temp is like {"default arg name": "default arg value"}
 	func_params = dict()
@@ -106,18 +146,18 @@ def _defer(all_params, reactorDetails):
 	try:
 		runtimeAns = eval("{}({})".format("func", the_args))
 	except Exception as e:
-		if reactorDetails.onerror:	pass
-		else:						raise e
- 	if runtimeAns == reactorDetails.expectedAns:
+		if reactorDetails.onerror:  pass
+		else:                       raise e
+	if runtimeAns == reactorDetails.expectedAns:
 		# inform pop
 	else:
 		errMsg = f"{func.__name__} didn't return expected value"
-		if reactorDetails.onerror:	print(errMsg)
-		else:						raise RuntimeError(errMsg)
+		if reactorDetails.onerror:  print(errMsg)
+		else:                       raise RuntimeError(errMsg)
 	return
 
 def defer(params, reactors):
-	"""	async await
+	""" async await
 	https://hackernoon.com/asynchronous-python-45df84b82434
 	https://gist.github.com/nhumrich/b53cc1e0482411e4b53d74bd12a485c1#file-gevent_example-py
 	https://medium.freecodecamp.org/a-guide-to-asynchronous-programming-in-python-with-asyncio-232e2afa44f6
@@ -141,7 +181,7 @@ def push(message, sender):
 	"""
 	message_id = str(uuid.uuid4())
 	jobs.append({message_id: message})
-	wake_everyone()	# HAHA
+	wake_everyone() # HAHA
 	reactor_list = physics_rules[sender]
 	defer(message, reactor_list)
 	return
@@ -151,7 +191,7 @@ def pop(message_id):
 	return
 
 def _know_that(sender, reactor_list):
-	"""	has the responsibility of calling pop()
+	""" has the responsibility of calling pop()
 	ans.subscribe(speaker)  # does this go into setup.py
 	ans.set(reaction, stimulant)    # one stimulant can have many reactions
 	"""
